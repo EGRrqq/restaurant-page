946800b89455638b50bed54952174a716091ef85
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-11-11 10:39:13 -0600

    feat(css): add basic layout styles and add a new sweet font

---
+++ src/inventory/InventoryItem.ts
@@ -0,0 +1,9 @@
+// import Food from "../../db/Food";
+//
+// export default class InventoryItem extends Food {
+//   #node: HTMLElement;
+//
+//   constructor(...args: Food[]) {
+//     super(...args);
+//   }
+// }
