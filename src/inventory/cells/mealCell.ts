b465aa66e1711b825f9282259283e2a7ee9d6687
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-11-23 03:15:28 -0600

    feat(ts): mealCell onClick to increase/decrease satiety

--- src/inventory/cells/mealCell.ts
+++ src/inventory/cells/mealCell.ts
@@ -1,35 +1,67 @@
-import { getSwapMeals } from "../../db";
+import { getSwapMeals, getVisitorMeal, getVisitorMeals } from "../../db";
 import cell from "./cell";
+import emptyCell from "./emptyCell";
 
 export default class mealCell extends cell {
   #id = Math.random().toString(36).substring(2);
+  #satiety: number;
 
-  constructor(src: string) {
+  constructor(src: string, satiety: number) {
     super();
 
     this.cell.style.background = `url(${src}) 100%/100% no-repeat, darkseagreen`;
     this.cell.dataset.type = "food";
+    this.cell.classList.add("meal");
     this.cell.id = this.id;
+    this.#satiety = satiety;
 
     this.cell.setAttribute("draggable", "true");
 
     this.cell.addEventListener("dragstart", this.cellDragStart);
-    this.cell.addEventListener("click", this.cellClick);
+    this.cell.addEventListener("click", this.cellSwapClick);
+    this.cell.addEventListener("click", this.cellProgressClick);
   }
 
   get id() {
     return this.#id;
   }
 
+  get satiety() {
+    return this.#satiety;
+  }
+
   cellDragStart(event: DragEvent) {
     event.dataTransfer.setData("element/id", (event.target as HTMLElement).id);
 
     event.dataTransfer.effectAllowed = "move";
-
-    console.log(event.dataTransfer);
   }
 
-  cellClick = (event: Event) => {
+  cellProgressClick = () => {
+    if (this.cell.parentElement.dataset.type === "store") return;
+
+    const getProgress = () => document.querySelector("progress");
+
+    if (getProgress().value <= 0 || getProgress().value >= 100) return;
+
+    getProgress().value += this.satiety;
+
+    const initPos = this.positionIndex;
+
+    const newCell = new emptyCell();
+
+    this.cell.replaceWith(
+      newCell.setPositionIndex(parseInt(initPos)).setAttributes().cell,
+    );
+
+    newCell.setParent();
+
+    getVisitorMeals().splice(
+      getVisitorMeals().indexOf(getVisitorMeal(this.id)),
+      1,
+    );
+  };
+
+  cellSwapClick = (event: Event) => {
     const getSwapBtn = () =>
       document.getElementById("swap-btn") as HTMLButtonElement;
 
@@ -43,8 +75,6 @@ export default class mealCell extends cell {
       if (!getSwapMeals().length) {
         getSwapBtn().setAttribute("disabled", "true");
       }
-
-      console.log("dataset", getSwapMeals());
     } else {
       if (this.cell.parentElement.dataset.type !== "store") return;
 
@@ -55,7 +85,6 @@ export default class mealCell extends cell {
       if (getSwapBtn().disabled) {
         getSwapBtn().removeAttribute("disabled");
       }
-      console.log(getSwapMeals());
     }
   };
 }
